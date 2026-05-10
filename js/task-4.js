27df349987778160dbb1d7d02bd34a507dcb4a59
Maks <pochtalion777@gmail.com>
2026-05-10 19:51:40 +0200

    created hw tasks 4-5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const formElem = document.querySelector('.login-form')
+// console.log(formElem);
 
+
+
+formElem.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const inputEmail = formElem.elements.email.value.trim();
+    // console.log(inputEmail);
+
+    const inputPassword = formElem.elements.password.value.trim();
+    // console.log(inputPassword);
+
+    if (inputEmail === '' || inputPassword === '') {
+        return alert('All form fields must be filled in');
+    }
+
+    const values = {
+        email: inputEmail,
+        password: inputPassword,
+    };
+
+    console.log(values);
+    
+    formElem.reset();
+});
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyElem = document.querySelector('body');
+console.log(bodyElem);
+
+const spanElem = bodyElem.querySelector('.color');
+console.log(spanElem);
+
+const btnElem = bodyElem.querySelector('.change-color');
+console.log(btnElem);
+
+btnElem.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  bodyElem.style.backgroundColor = newColor;
+  spanElem.textContent = newColor;
+});
